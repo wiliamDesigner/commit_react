7e6857d646fc3956dc60b5c5eb982808391da828
wiliamDesigner <wiliam.omura3@hotmail.com>
2022-10-04 15:26:49 -0300

    primeiro commit do react

---
+++ app01/src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Edite <code>src/App.js</code> e salve e carregue
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          aprender react js
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
